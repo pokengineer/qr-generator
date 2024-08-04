3f976e9beb89dee78280d8e968d51169e6bc4e71
facuv <vfacuu@gmail.com>
2024-08-03 22:31:02 -0300

    added validation to for txt inputs

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from tkinter import *
 import qrcode
+from PIL import ImageTk, Image
 
 def create_and_save_qr( data, filename ='test.png'):
     img = qrcode.make( str(data) )
     img.save( filename )
 
 def clicked():
-    create_and_save_qr( txt.get(), txt2.get())
+    """
+    click event on the button. validates inputs and calls image creator function.
+    """
+    text = txt.get()
+    if len(text) == 0:
+        error = Label(window, text="text is empty")
+        error.grid(column=1, row=5)
+        return
+    filename = txt2.get()
+    if len(filename) == 0:
+        filename = 'test.png'
+    create_and_save_qr( txt.get(), filename )
 
 
 if __name__ == '__main__':
@@ -25,6 +37,7 @@ if __name__ == '__main__':
     lbl2.grid(column=0, row=1)
 
     txt2 = Entry(window,width=50)
+    txt2.insert(0, 'test.png') 
     txt2.grid(column=1, row=1)
 
 
